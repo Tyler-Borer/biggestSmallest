8b761a2d0561347f6eea70e12f251de9b8dc06c1
tborer <tborer22@gmail.com>
2016-07-23 14:15:28 -0700

    Update biggestSmallest.js

--- biggestSmallest.js
+++ biggestSmallest.js
@@ -1,16 +1,16 @@
 var arr = [2, 4, 6, 9, 7, 3,];
 
-max = Math.max.apply(arr[0], arr);
-min = Math.min.apply(arr[0], arr);
+max = Math.max.apply(arr[0], arr); 				// search entire array for the biggest number
+min = Math.min.apply(arr[0], arr); 				// search entire array for the smallest number
 
 function newArr (arr){
-for (var i = 0; i < arr.length; i++){
-var newArr = [Math.min.apply(arr[0], arr), Math.max.apply(arr[0], arr)];
+for (var i = 0; i < arr.length; i++){ 				// iterate through the array cells
+var newArr = [min, max];  					// set the new array to these cells
 
 }
 return newArr;
 }
-newArr(arr);
+newArr(arr);							// call newArr
 
 
 
